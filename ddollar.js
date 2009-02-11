56f51889efbfab94b0e57181906bc1b296d10675
David Dollar <ddollar@gmail.com>
2009-02-11 00:29:00 -0500

    more work to my page

--- ddollar.js
+++ ddollar.js
@@ -1,23 +1,17 @@
 function dynamize() {
-
-  repository_list = document.createElement('ul');
+  var my_code = $('#my_code');
+  var forked_code = $('#forked_code');
 
   GitHub.User.fetch('ddollar', function(user) {
-    $.each(user.repositories, function(id, repo) {
-      if (!repo.fork) {
-        repository_item = document.createElement('li');
-        repository_item.className = 'repository_item';
+    $.each(user.repositories,  function(id, repo) {
+      var repository_item       = document.createElement('li');
+      repository_item.className = 'repository';
+      var repository_link      = document.createElement('a');
+      repository_link.href      = repo.url;
+      repository_link.innerHTML = repo.name;
+      repository_item.appendChild(repository_link);
 
-        repository_link = document.createElement('a');
-        repository_link.href = repo.url;
-        repository_link.innerHTML = repo.name;
-        repository_item.appendChild(repository_link);
-
-        repository_list.appendChild(repository_item);
-      }
+      (repo.fork ? forked_code : my_code).prepend(repository_item);
     });
-    console.log(user);
   });
-
-  $('#repositories').append(repository_list);
 }
